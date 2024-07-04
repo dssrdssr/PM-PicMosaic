31f8d7f160a2778b29613dbd8dd6e5f0e7aa47ca
yudenso <62161274+yudenso@users.noreply.github.com>
2024-07-04 21:05:56 +0800

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Test/>
+        <Hitokoto/>
+    </div>
+</template>
+
+<script>
+import Test from "@/components/Test";
+import Hitokoto from "@/components/Hitokoto";
+import Nav from "@/components/Nav";
+    export default {
+        name:'Home',
+        components:{
+            Test,
+            Hitokoto,
+            Nav
+        }
+    }
+</script>
+
+<style  scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/pages/Home.vue'
+import Heros from '@/pages/Login.vue'
+import Function from '@/pages/Function.vue'
+import HerosDetail from '@/pages/HerosDetail.vue'
+
+Vue.use(VueRouter)
+
+// 创建路由实例对象 
+export default new VueRouter({
+    // 路由  key（url地址）  ----> value（组件）
+    routes:[   // 路由 配置
+        {
+            path:"/",  //路由地址
+            component:Home,  //路由对应的组件
+            meta:{  //路由元信息
+                name:"首页",   // 路由导航名称
+                isShow:true
+            }
+        },
+        {
+            path:"/login",
+            component:Heros,
+            meta:{
+                name:"登录",
+                isShow:true
+            }
+        },
+        {
+            path:"/function",
+            component:Function,
+            meta:{
+                name:"使用",
+                isShow:true
+            }
+        }
+    ]
+})
