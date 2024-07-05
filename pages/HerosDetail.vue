d4bea0303b2ce53db1d1ca0e236ec71d9d588253
yudenso <62161274+yudenso@users.noreply.github.com>
2024-07-05 10:01:39 +0800

    Add files via upload

---
+++ pages/HerosDetail.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="heros-con">
+       <div class="heros-left">
+           <img :src="`http://cdn.xiaohigh.com/${hero.image}`" alt="" />
+           <h2>{{hero.name}}</h2>
+           <h3>{{hero.type}}</h3>
+           <div class="img-min">
+               <ul>
+                   <li v-for="(item,index) in hero.skills" :key="index">
+                       <img :src="`http://cdn.xiaohigh.com/${item.img}`" alt="" />
+                       <h4>{{item.name}}</h4>
+                   </li>
+                   
+               </ul>
+           </div>
+       </div>
+       <div class="heros-right">
+           <img :src="`http://cdn.xiaohigh.com/${hero.big_image}`" alt="" />
+           <hr />
+           <p v-html="hero.intro"></p>
+       </div>
+   </div>
+</template>
+
+<script>
+   export default {
+       name:"HerosDetail",
+       data(){
+           return{
+               hero:{}
+           }
+       },
+       async mounted(){
+           // console.log(this.$route.query.id);
+
+           let {data} = await this.$axios.get(`http://api.xiaohigh.com/heros/${this.$route.query.id}`);
+           // console.log(data);
+           this.hero = data;
+       }
+   }
+</script>
+
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
---
+++ pages/Login.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <br/><br/>
+            <input v-model="keyword" type="text" placeholder="Id" />
+            <br/><br/><br/>
+            <input v-model="keyword" type="text" placeholder="Password" />
+            <br/><br/><br/>
+            <ul>
+            <li>
+            <button @click="login">登录</button>
+            </li>
+            <li>
+            <button @click="register">注册</button>
+            </li>
+        
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Heros",   //组件名
+    data() {
+        return {
+            // 表单数据对象
+            loginForm: {
+            username: "admin",
+            password: "123456",
+            }
+        }
+    },
+    methods:{   // 普通函数 方法
+        async login(){
+            console.log(this);
+        },
+        async register(){
+            console.log(this);
+        }
+    }
+}
+</script>
+
+<style scoped>
+ .login_container{
+        text-align:center;
+        background-color: lightcoral;
+        height: 100%;
+    }
+.login_box {
+    width: 450px;
+    height: 250px;
+    background: #e4f12b;
+    border-radius: 3px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+ul{
+    list-style: none;
+    background: rgb(42, 153, 180);
+    color: white;
+    font-size: 30px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    height: 60px;
+    line-height: 40px;
+   }
+
+   ul>li{
+    display: inline-block;
+    width: 20%;
+    text-align: center;
+   }
+</style>
