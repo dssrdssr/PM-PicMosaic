710f6b5b2bd654b7f1777d3d310838afa1c6ae2f
yudenso <62161274+yudenso@users.noreply.github.com>
2024-07-12 10:31:44 +0800

    Update mosaic.py

--- 后端/sql_app/mosaic.py
+++ 后端/sql_app/mosaic.py
@@ -20,45 +20,46 @@ class MosaData(BaseModel):
 #对单张图片处理
 def mosa(name:str,location_set:list[list[int]],style:int = 1,mosasize:int = 30):
     lena = cv2.imread(name,cv2.IMREAD_COLOR)
-
-    location_set[0]
-    row, colume,color=lena.shape
-    mask=np.zeros((row,colume,color),dtype=np.uint8)
-    for e in location_set:
-        # mask[e[0]:e[2],e[1]:e[3]]=1   #传入参数为 x1 y1 x2 y2
-        mask[e[0]:e[0]+int(1.1*e[3]),e[1]-int(0.3*e[2]):e[1]+e[2]] = 1 # 传入参数top,left,width,height
+    if lena:
+        location_set[0]
+        row, colume,color=lena.shape
+        mask=np.zeros((row,colume,color),dtype=np.uint8)
+        for e in location_set:
+            # mask[e[0]:e[2],e[1]:e[3]]=1   #传入参数为 x1 y1 x2 y2
+            mask[e[0]:e[0]+int(1.1*e[3]),e[1]-int(0.3*e[2]):e[1]+e[2]] = 1 # 传入参数top,left,width,height
    
     
-    if style==1:#雪花屏
-        key=np.random.randint(0,256,size=[row,colume,color],dtype=np.uint8)
-        lenaXorKey=cv2.bitwise_xor(lena,key)
-        encryptFace=cv2.bitwise_and(lenaXorKey,mask*255)
-    if style==2:#纯白
-        encryptFace=mask*255
-    if style==3:#纯黑
-        encryptFace=mask
-    if style==4:#变模糊
-        temp_img = cv2.resize(lena,dsize=(10,10))
-        temp_img = cv2.resize(temp_img,dsize=(colume,row))
-        encryptFace = cv2.bitwise_and(temp_img,mask*255)
-    if style==5:#马赛克
-        temp_img=lena[::mosasize,::mosasize]
-        temp_img = cv2.resize(temp_img,dsize=(colume,row),interpolation=0)#最近邻差值
-        # print(mask.shape)
-        # print(row," ",colume)
-        # print(temp_img.shape)
-        encryptFace = cv2.bitwise_and(temp_img,mask*255)
+        if style==1:#雪花屏
+            key=np.random.randint(0,256,size=[row,colume,color],dtype=np.uint8)
+            lenaXorKey=cv2.bitwise_xor(lena,key)
+            encryptFace=cv2.bitwise_and(lenaXorKey,mask*255)
+        if style==2:#纯白
+            encryptFace=mask*255
+        if style==3:#纯黑
+            encryptFace=mask
+        if style==4:#变模糊
+            temp_img = cv2.resize(lena,dsize=(10,10))
+            temp_img = cv2.resize(temp_img,dsize=(colume,row))
+            encryptFace = cv2.bitwise_and(temp_img,mask*255)
+        if style==5:#马赛克
+            temp_img=lena[::mosasize,::mosasize]
+            temp_img = cv2.resize(temp_img,dsize=(colume,row),interpolation=0)#最近邻差值
+            # print(mask.shape)
+            # print(row," ",colume)
+            # print(temp_img.shape)
+            encryptFace = cv2.bitwise_and(temp_img,mask*255)
 
                
     
-    noFacel=cv2.bitwise_and(lena, (1-mask)*255)
-    maskFace=encryptFace+noFacel
+        noFacel=cv2.bitwise_and(lena, (1-mask)*255)
+        maskFace=encryptFace+noFacel
 
-    # cv2.imshow("origin",lena)
-    # cv2.imshow("after",maskFace)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
-    return maskFace
+        # cv2.imshow("origin",lena)
+        # cv2.imshow("after",maskFace)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+        return maskFace
+    return 0
 
 #从input批量处理输出到output
 def mul_mosaic(mosadata:MosaData):
