97ef898252fcb5687827a36eaf4d74ef8bb4c324
yudenso <62161274+yudenso@users.noreply.github.com>
2024-07-12 10:01:39 +0800

    Update models.py

--- 后端/sql_app/models.py
+++ 后端/sql_app/models.py
@@ -4,17 +4,26 @@ from .database import Base # 假设这里已经定义了Base基类
  
 class User(Base): 
     __tablename__ = "usr" 
-    uname = Column(String,primary_key=True)
+    username = Column(String,primary_key=True)
     password = Column(String)  
     # 0被封号  
     is_active = Column(String, default='1') 
     # 0为普通用户，1为DBA，2为程序员用户  
     authority = Column(String, default='0') 
     pics = relationship("Pic", back_populates="owner") 
+    words = relationship("Word", back_populates="owner") 
  
 class Pic(Base): 
     __tablename__ = "pic" 
     picid = Column(Integer, primary_key=True, autoincrement=True)
     picname = Column(String) 
-    owner_id = Column(String, ForeignKey("usr.uname")) # 相应地更改ForeignKey的类型  
+    owner_id = Column(String, ForeignKey("usr.username")) # 相应地更改ForeignKey的类型  
     owner = relationship("User", back_populates="pics") 
+
+
+class Word(Base):
+    __tablename__ = "word"
+    owner_id = Column(String, ForeignKey("usr.username"),primary_key=True)
+    name=Column(String,primary_key=True)
+    word=Column(String,primary_key=True)
+    owner = relationship("User", back_populates="words")
